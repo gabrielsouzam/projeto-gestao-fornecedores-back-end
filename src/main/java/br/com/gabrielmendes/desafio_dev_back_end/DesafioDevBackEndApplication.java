3be4089303c1aabf6dce30df019d442ecf821e58
gabrielmendes64 <gabriel10010100@gmail.com>
2024-08-25 21:38:23 -0300

    chore: add spring boot configuration

---
+++ src/main/java/br/com/gabrielmendes/desafio_dev_back_end/DesafioDevBackEndApplication.java
@@ -0,0 +1,13 @@
+package br.com.gabrielmendes.desafio_dev_back_end;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioDevBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioDevBackEndApplication.class, args);
+	}
+
+}
